f99e9d1d4bdab1a77d71cdadcaa1cb2d5c307b20
jiin972 <jiin972@gmail.com>
2026-03-23 03:55:16 +0000

    feat:Flask를 활용한 data 스크래핑 및 메모리 캐시 구현 - "get"을 통해홈페이지, 서치페이지 렌더링 구 - "keyword"를 통한 data 스크래핑 후, 결과를 "db"(dict)에 저장 - 캐시 저장된 data를 CSV파일로 저장

--- app.py
+++ app.py
@@ -1,16 +1,48 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect,send_file
+from scraper import WantedScraper
+from file import save_to_file
+
 
 app = Flask("JobScraper")
+
+
+db = {} #가짜 데이터베이스 생성(캐시역할, 딕셔너리)
 
 @app.route("/")
 def home():
     return render_template("home.html", name = "jin")
 
 @app.route("/search")
-def hello():
-    keyword = request.args.get("keyword")
-    return render_template("search.html", name="jin" , keyword=keyword)
-    
+def search():
+    keyword = request.args.get("keyword") #request.args의 data에서 필요한 부분은 .get()함수로 추출
+    #키워드 없을 때, 방어코드 작성
+    if keyword == None:
+        return redirect("/")
+
+    if keyword in db:
+        jobs=db[keyword]
+    else:
+        #인스턴스 생성
+        scraper = WantedScraper(keyword)
+        #결과 리스트 받기
+        jobs = scraper.get_page()
+        db[keyword] = jobs
+    return render_template("search.html", name="jin" , keyword=keyword, jobs = jobs)
+        
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword") #request.args의 data에서 필요한 부분은 .get()함수로 추출
+    #키워드 없을 때, 방어코드 작성
+    if keyword == None:
+        return redirect("/")
+    #db에 없는 키워드 접근, 방어코드 작성
+    if keyword is not db:
+        return redirect(f"/search?keyword={keyword}") #키워드가 없을 경우, 검색페이지 강제 이동
+    #파일저장로직
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
+
 
 
 #서버작동, 맨 마지막에 실행
