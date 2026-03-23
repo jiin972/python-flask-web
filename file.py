34f380e941798bcc41f4d6e5261c70e7c1539f0e
jiin972 <jiin972@gmail.com>
2026-03-23 04:42:59 +0000

    fix: 파일 저장로직 수정 - 확장자 지정 - 한글 인코딩 추가

--- file.py
+++ file.py
@@ -2,7 +2,7 @@ import csv
 
 def save_to_file(file_name, jobs_db):
         # 스크래핑 결과로 CSV파일 생성
-    file = open(f"{file_name}","w") #open함수, 파일열기(쓰기모드)
+    file = open(f"{file_name}.csv","w", encoding="utf-8-sig") #open함수, 파일열기(쓰기모드)
     writer = csv.writer(file) #writer 생성(cvs)
     writer.writerow(["Title","Company","Career", "Reward","Link"]) #List 넘겨줘야함, 헤더작성
     for job in jobs_db: # 헤더에 맞는 데이터 작성(딕셔너리의 values만추출)
